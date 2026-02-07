600aaa4c5366582b4fbaed8ae3c3177f619fbc5a
AlexandreHoumeau <alexandre.houmeau@gmail.com>
2026-02-07 19:02:36 +0100

    created function to generate slug by opportunity

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,38 @@
+import { SupabaseClient } from "@supabase/supabase-js";
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+
+export function generateSlug(title: string): string {
+  return title
+    .toLowerCase()
+    .normalize("NFD") // Supprime les accents
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^a-z0-9\s-]/g, "") // Supprime les caractères spéciaux
+    .trim()
+    .replace(/\s+/g, "-") // Remplace les espaces par des tirets
+    .replace(/-+/g, "-"); // Supprime les tirets multiples
+}
+
+export async function getUniqueSlug(
+  supabase: SupabaseClient,
+  baseSlug: string,
+  table: string = "opportunities",
+): Promise<string> {
+  let slug = baseSlug;
+  let count = 1;
+  while (true) {
+    const { data } = await supabase
+      .from(table)
+      .select("slug")
+      .eq("slug", slug)
+      .single();
+    if (!data) break;
+    slug = `${baseSlug}-${count++}`;
+  }
+  return slug;
 }
