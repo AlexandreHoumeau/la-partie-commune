81760bde30ac6ae14a0350d2adc974ec339b165a
AlexandreHoumeau <alexandre.houmeau@gmail.com>
2026-02-09 22:05:08 +0100

    small ui fix

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -9,7 +9,7 @@ export default function AppLayout({
     <div className="h-screen bg-neutral-50">
       <AppSidebar />
 
-      <main className="ml-64 overflow-y-auto p-6 bg-neutral-50">
+      <main className="ml-64 overflow-y-auto bg-neutral-50">
         {children}
       </main>
     </div>
